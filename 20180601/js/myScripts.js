ffa878685bb709bed7d7e1dec0ae8c29dfccddaa
babulitl240618 <babul.itl240618@gmail.com>
2018-07-01 17:11:48 +0600

    Javascript with object

--- 20180601/js/myScripts.js
+++ 20180601/js/myScripts.js
@@ -1,3 +1,13 @@
 function myFunction(){
 	document.getElementById("demo").innerHTML="Change Paragraph";
 }
+
+var person={
+	name : "Babul Miah",
+	age: 25,
+	address: "Jamalpur"
+};
+
+function showPersonInfo(){
+	document.getElementById("personInfo").innerHTML=person.name+" is "+person.age+" years old and address is "+person.address;
+}
